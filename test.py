d8b8906a9942e5516d65cf9032e0dee232430de2
samsick512 <72693014+samsick512@users.noreply.github.com>
2020-10-11 22:12:07 +0900

    Create test.py
    파이썬 코드 테스트

---
+++ test.py
@@ -0,0 +1,20 @@
+
+def findMAx(a,b,c):
+    if a>b:
+        biggest=a
+    else:
+        biggest=b
+
+    if c>biggest:
+        biggest=c
+
+    return biggest
+
+
+a=int(input("첫 번째 숫자 입력:"))
+b=int(input("두 번째 숫자 입력:"))
+c=int(input("세 번째 숫자 입력:"))
+
+biggest= findMAx(a,b,c)
+
+print(a,b,c,"중 가장큰 수는", biggest,"입니다.")
